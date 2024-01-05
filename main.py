c2698b7b4ff6960ef0f5fc8512bc4e72cb28f167
Bernardo6 <bernardo98@students.uonbi.ac,ke>
2024-01-05 18:34:42 +0300

    updates

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 
 app = FastAPI()
 
@@ -12,7 +13,7 @@ class Post(BaseModel):
     Rating: Optional[int] = None
 
 
-my_posts =[{'Title': 'Top ten Technologies to learn in 2024', 'Content': 'Look at this tech that will change your life in 2024', 'Published': True, 'Rating': None}, {'Title': 'Top ten artist in 2024', 'Content': 'Look at this best music in 2024', 'Published': True, 'Rating': None}]
+my_posts =[{'Title': 'Top ten Technologies to learn in 2024', 'Content': 'Look at this tech that will change your life in 2024', 'Published': True, 'Rating': None, "id": 1}, {'Title': 'Top ten artist in 2024', 'Content': 'Look at this best music in 2024', 'Published': True, 'Rating': None, "id": 2}]
 
 @app.get("/")
 async def root():
@@ -28,13 +29,21 @@ async def get_posts():
 #     print(payload)
 #     return {"new_post": f"title: {payload['Title']} Content: {payload['Content']}"}
 @app.post("/posts")
-async def create_post(new_post: Post):
+async def create_post(post: Post):
     
     # print(f"Title: {new_post.Title}")
     # print(f"content: {new_post.Content}")
     # print(f"Publish: {new_post.Published}")
     # print(f"Rating: {new_post.Rating}")
-    print(new_post)
-    print(new_post.dict())
     
-    return {"data" :"new_post"}
+    post_dict = post.dict()
+    post_dict['id']=randrange(0,1000000)
+    my_posts.append(post_dict)
+    
+    
+    return {"data" : post_dict}
+
+@app.get("/posts/{id}")
+def get_post(id):
+    print(id)
+    return {"post_detail": f"Here is a retrieved post of id {id}"}
